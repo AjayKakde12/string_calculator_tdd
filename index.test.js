db25a1693d9f83708033548122e3a7a0a52ca1ca
Ajay Kakde <ajay.kakde@iauro.com>
2023-02-06 16:55:04 +0530

    Negative Test Case: Where input string is 1,2

--- index.test.js
+++ index.test.js
@@ -8,5 +8,8 @@ describe("To calculate addition of numbers in given string", function() {
 
         addition = await stringCalculator("1");
         expect(addition).to.equals(1);
+
+        addition = await stringCalculator("1,2");
+        expect(addition).to.equals(3);
     })
 })
