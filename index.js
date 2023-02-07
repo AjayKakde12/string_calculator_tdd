c7a509cc10b707de0d30659f12ad770c0bfb34b7
Ajay Kakde <ajay.kakde@iauro.com>
2023-02-07 23:57:09 +0530

    Positive Test Case 1: Where input string is //

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 module.exports = {
     stringCalculator: async (numberString) => {
+        numberString = numberString.replace(/\n/, ",");
         if(numberString.length === 0) {
             return 0;
         }
         if(numberString.length === 1) {
             return Number(numberString);
         }
-        numberString = numberString.replace(/\n/, ",");
-        numberString = numberString.replace(';', ",");
+        let findDelimiter = numberString.split("//");
+        let delimiter = findDelimiter[1] ? findDelimiter[1][0] : null;
+        if(delimiter !== null) {
+            numberString = numberString.replaceAll(delimiter, ",");
+        }
         const numbers = numberString.split(",");
+        if(numbers[numbers.length - 1] === '') {
+            return "Not Valid"
+        }
         let sum = 0;
         for(let number of numbers) {
-            if(number === '') {
-                return "Not Valid";
+            if(number.match(/\d/)) {
+                sum += Number(number);
             }
-            sum += Number(number);
         }
         return sum;
     }
