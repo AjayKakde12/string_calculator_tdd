21978b30b47e6233b5e231db52d7e17612b41e3b
Ajay Kakde <ajay.kakde@iauro.com>
2023-02-08 00:39:36 +0530

    Positive Test Case: i/p is -1,-2,-3

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
 module.exports = {
     stringCalculator: async (numberString) => {
-        numberString = numberString.replaceAll(/\n/g, ",");
-        if(numberString.length === 0) {
-            return 0;
-        }
-        if(numberString.length === 1) {
-            return Number(numberString);
-        }
-        let findDelimiter = numberString.split("//");
-        let delimiter = findDelimiter[1] ? findDelimiter[1][0] : null;
-        if(delimiter !== null) {
-            numberString = numberString.replaceAll(delimiter, ",");
-        }
-        const numbers = numberString.split(",");
-        if(numbers[numbers.length - 1] === '') {
-            return "Not Valid"
-        }
-        console.log(numberString);
-        let sum = 0;
-        for(let number of numbers) {
-            if(number.match(/\d/)) {
-                sum += Number(number);
+        try {
+            let negativeNumbers = [];
+            numberString = numberString.replaceAll(/\n/g, ",");
+            if(numberString.length === 0) {
+                return 0;
             }
+            if(numberString.length === 1) {
+                return Number(numberString);
+            }
+            let findDelimiter = numberString.split("//");
+            let delimiter = findDelimiter[1] ? findDelimiter[1][0] : null;
+            if(delimiter !== null) {
+                numberString = numberString.replaceAll(delimiter, ",");
+            }
+            const numbers = numberString.split(",");
+            if(numbers[numbers.length - 1] === '') {
+                return "Not Valid"
+            }
+            let sum = 0;
+            for(let number of numbers) {
+                if(Number(number) < 0) {
+                    negativeNumbers.push(number);
+                }
+                if(number.match(/\d/)) {
+                    sum += Number(number);
+                }
+            }
+            if(negativeNumbers.length) {
+                throw `negatives not allowed: ${negativeNumbers.join(',')}`;
+            } 
+            return sum;
+        } catch(msg) {
+            return new Error(msg)
         }
-        return sum;
     }
 }
